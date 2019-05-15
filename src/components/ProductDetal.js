1959fd7d917510d566be8a577e47d7a078992056
Michael Snowden <mtsnowden@gmail.com>
2019-05-15 08:28:35 -0500

    working on *send prop called product

---
+++ src/components/ProductDetal.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function ProductDetail() {
+    return (
+        <div className="grid_1_of_5 images_1_of_5">
+									<img src="images/g1.jpg"/>
+									<h3>Lorem Ipsum is simply </h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+									<h4><span>$600.00</span>$512.00</h4>
+									<div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+								</div>
+
+								<div className="grid_1_of_5 images_1_of_5">
+										<img src="images/g6.jpg" />
+										<h3>Lorem Ipsum is simply </h3>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+									<h4><span>$400.00</span>$352.00</h4>
+									<div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+								</div>
+
+								<div className="grid_1_of_5 images_1_of_5">
+									<img src="images/g7.png" />
+										<h3>Lorem Ipsum is simply </h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+										<h4><span>$300.00</span>$202.00</h4>
+											<div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+								</div>
+
+								<div className="grid_1_of_5 images_1_of_5">
+										<img src="images/g8.png" />
+										<h3>Lorem Ipsum is simply </h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+										<h4><span>$400.00</span>$322.00</h4>
+											<div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+								</div>
+
+								<div className="grid_1_of_5 images_1_of_5">
+										<img src="images/g1.jpg" />
+										<h3>Lorem Ipsum is simply</h3>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
+										<h4><span>$700.00</span>$602.00</h4>
+											<div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+                                </div>);
+}
+
+export default ProductDetail;
